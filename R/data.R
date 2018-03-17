4297d93dd7c28a5afb94e554a427750a570e44c5
Johan Larsson <johanlarsson@outlook.com>
2018-03-17 22:16:05 +0000

    update datasets

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' A benchmark of L2-regularized logistic regression in the
 #' \pkg{bigoptim} and \pkg{glmnet} packages based on subsets of the
-#' [bigoptim::covtype.libsvm] dataset.
+#' [covtype] dataset.
 #' @format A data frame with 800 rows and 5 variables:
 #' \describe{
 #'   \item{rows}{number of rows in data}
@@ -11,20 +11,59 @@
 #'   \item{loss}{value of objective function}
 #'   \item{package}{package}
 #' }
+#' @source https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
 "data_medium"
 
 #' Benchmarks for medium-hard task
 #'
-#' A benchmark of L1-regularized logistic regression in the
-#' \pkg{scikit-learn} python package and \pkg{glmnet} R package based
-#' on subsets of the [bigoptim::covtype.libsvm] dataset.
+#' A benchmark of SAGA L1-regularized logistic regression in the
+#' \pkg{scikit-learn} python package and the python version of
+#' \pkg{glmnet} based
+#' on subsets of the [covtype], [ijcnn1], [a9a], [mushrooms],
+#' [skin_noskin], and [phishing] datasets.
 #'
 #' @format A data frame with 800 rows and 5 variables:
 #' \describe{
-#'   \item{rows}{number of rows in data}
-#'   \item{cols}{number of columns in data}
-#'   \item{time}{time in nanoseconds}
+#'   \item{dataset}{dataset}
+#'   \item{package}{package, glmnet or saga}
+#'   \item{time}{runtime in seconds}
 #'   \item{loss}{value of objective function}
-#'   \item{package}{package}
 #' }
+#' @source https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
 "data_mediumhard"
+
+#' ijcnn1
+#'
+#' @format A dataset of 49,990 observations and 22 features.
+#' @source https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
+"ijcnn1"
+
+#' a9a
+#'
+#' @format A dataset of 32,561 observations and 123 features.
+#' @source https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
+"a9a"
+
+#' phishing
+#'
+#' @format A dataset of 11,055 observations and 68 features.
+#' @source https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
+"phishing"
+
+#' mushrooms
+#'
+#' @format A dataset of 8,124 observations and 112 features.
+#' @source https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
+"mushrooms"
+
+#' covtype
+#'
+#' @format A dataset of 581,012 observations and 54 features.
+#' @source https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
+"covtype"
+
+#' skin_nonskin
+#'
+#' @format A dataset of 245,057 observations and 3 features.
+#' @source https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
+"skin_nonskin"
--- data-raw/datasets.R
+++ data-raw/datasets.R
@@ -5,7 +5,7 @@ download.file(
   "ijcnn1.bz2"
 )
 ijcnn1 <- e1071::read.matrix.csr(bzfile("ijcnn1.bz2"), fac = FALSE)
-devtools::use_data(ijcnn1, overwrite = TRUE, internal = TRUE)
+devtools::use_data(ijcnn1, overwrite = TRUE)
 unlink("ijcnn1.bz2")
 
 # a9a ---------------------------------------------------------------------
@@ -15,7 +15,7 @@ download.file(
   "a9a"
 )
 a9a <- e1071::read.matrix.csr("a9a", fac = FALSE)
-devtools::use_data(a9a, overwrite = TRUE, internal = TRUE)
+devtools::use_data(a9a, overwrite = TRUE)
 unlink("a9a")
 
 # phishing ----------------------------------------------------------------
@@ -25,18 +25,19 @@ download.file(
   "phishing"
 )
 phishing <- e1071::read.matrix.csr("phishing", fac = FALSE)
-devtools::use_data(phishing, overwrite = TRUE, internal = TRUE)
+devtools::use_data(phishing, overwrite = TRUE)
 unlink("phishing")
 
-# w8a ---------------------------------------------------------------------
+
+# mushrooms ---------------------------------------------------------------
 
 download.file(
-  "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/w8a",
-  "w8a"
+  "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/mushrooms",
+  "mushrooms"
 )
-w8a <- e1071::read.matrix.csr("w8a", fac = FALSE)
-devtools::use_data(w8a, overwrite = TRUE, internal = TRUE)
-unlink("w8a")
+mushrooms <- e1071::read.matrix.csr("mushrooms", fac = FALSE)
+devtools::use_data(mushrooms, overwrite = TRUE)
+unlink("mushrooms")
 
 # covtype -----------------------------------------------------------------
 
@@ -45,27 +46,15 @@ download.file(
   "covtype.libsvm.binary.scale.bz2"
 )
 covtype <- e1071::read.matrix.csr(bzfile("covtype.libsvm.binary.scale.bz2"), fac = FALSE)
-devtools::use_data(covtype, overwrite = TRUE, internal = TRUE)
+devtools::use_data(covtype, overwrite = TRUE)
 unlink("covtype.libsvm.binary.scale.bz2")
 
-#
-# # susy --------------------------------------------------------------------
-#
-# download.file(
-#   "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/SUSY.bz2",
-#   "SUSY.bz2"
-# )
-# susy <- e1071::read.matrix.csr(bzfile("SUSY.bz2"), fac = FALSE)
-# susy <- list(X = SparseM::as.matrix(susy$x), y = susy$y)
-# devtools::use_data(susy)
-# unlink("SUSY.bz2")
-
-# skin --------------------------------------------------------------------
+# skin_nonskin -------------------------------------------------------------
 
 download.file(
   "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/skin_nonskin",
   "skin_nonskin"
 )
-skin <- e1071::read.matrix.csr("skin_nonskin", fac = FALSE)
-devtools::use_data(skin, overwrite = TRUE, internal = TRUE)
+skin_nonskin <- e1071::read.matrix.csr("skin_nonskin", fac = FALSE)
+devtools::use_data(skin_nonskin, overwrite = TRUE)
 unlink("skin_nonskin")
